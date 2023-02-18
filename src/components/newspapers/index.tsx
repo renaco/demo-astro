b4ac9a84de22b78bccc0fc464e52f62475fe085d
renaco <renanromero@gmail.com>
2023-02-17 22:54:27 -0500

    feature: define flotead form

--- src/components/newspapers/index.tsx
+++ src/components/newspapers/index.tsx
@@ -37,15 +37,20 @@ const NewsPapers = () => {
         Selecciona los boletines que quieres recibir y haz clic en Registrar. Si
         eres suscriptor inicia sesión para recibir los Newsletter Plus.
       </p>
+      <hr
+        className="p-0 mx-0 mt-6 mb-0 h-0 text-black rounded-none border-b-0 border-t border-solid border-x-0 border-sky-800"
+        style={{ outline: 0, borderWidth: 0 }}
+      />
       <div
-        className="grid gap-4 p-0 mt-5 w-full text-black"
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-0 mt-5 w-full text-black"
         id="newsletter_paywall"
-        style={{ gridTemplateColumns: "1fr 1fr 1fr 1fr", height: 910, outline: 0 }}
+        style={{ height: 910, outline: 0 }}
       >
         {listContentArray.map((summary, index) => (
           <div
-            className="relative py-2 px-4 my-0 mx-auto text-black bg-white rounded-sm border border-solid border-neutral-200"
+            className="relative py-2 px-4 my-0 mx-auto text-black bg-white rounded-sm border border-solid border-neutral-200 w-full"
             style={{ height: 270, transition: "all 0.4s ease-out 0s" }}
+            key={index}
           >
             <a
               href=""
@@ -95,9 +100,48 @@ const NewsPapers = () => {
                 gridTemplateColumns: "15px 1fr",
                 right: 11
               }}
-            >+</button>
+            ><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none"><path fill="#000" d="M8.4 5.6h4.317v2.8H8.4v4.317H5.6V8.4H1.283V5.6H5.6V1.283h2.8V5.6Z" /></svg></button>
           </div>
         ))}
+      </div>
+      <div
+        className="flex fixed bottom-0 left-0 justify-center items-center py-6 px-0 my-0 mx-auto w-full h-24 font-sans text-center text-white border-t border-white border-dashed"
+        style={{
+          maxWidth: 1100,
+          transition: "all 1s ease 0s",
+          zIndex: 9,
+          backgroundImage:
+            "linear-gradient(270deg, rgba(69, 101, 133, 0.9) 0%, rgba(47, 133, 161, 0.9) 50%, rgba(0, 96, 102, 0.9) 100%)"
+        }}
+      >
+        <form
+          className="inline-flex relative gap-4 place-content-center py-2 px-0 my-0 mr-0 ml-4 text-center text-white"
+          id="formIdNews"
+          style={{ gridTemplateColumns: "1fr 1fr",  }}
+        >
+          <input
+            className="p-1 m-0 w-full h-[36px] font-sans text-sm text-black rounded border border-solid cursor-text border-neutral-400
+            "
+            type="text"
+            placeholder="Ingrese su E-mail"
+            required
+            id="email_news"
+            style={{ outline: 0, textIndent: 2 }}
+          />
+          <button
+            type="submit"
+            value=""
+            className="py-2 px-4 m-0 w-5/6 font-sans text-sm tracking-wider text-center text-white bg-teal-800 rounded border border-solid border-slate-400 hover:cursor-pointer"
+            style={{ outline: 0, transition: "all 0.4s ease-out 0s" }}
+          >
+            Registrar
+          </button>
+        </form>
+        <p
+          className="inline-block p-0 m-0 tracking-wide text-center text-white"
+          style={{ outline: 0 }}
+        ></p>
+
       </div>
     </div>
 
